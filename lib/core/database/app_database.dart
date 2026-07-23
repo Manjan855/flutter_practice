951c243af98ad732b5290ccf61562d5576c91811
Manjan855 <shahimanjan08@gmail.com>
2026-07-23 23:30:50 +0545

    sqflite

---
+++ lib/core/database/app_database.dart
@@ -0,0 +1,25 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class AppDatabase {
+  static Database? _database;
+  static Future<Database> get instance async {
+    if (_database != null) return _database!;
+    _database = await _initDb();
+    return _database!;
+  }
+
+  static Future<Database> _initDb() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'flutter_practice');
+    return openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute(
+          '''CREATE TABLE products(id INTERGER PRIMARY KEY, tittle TEXT , price REAL, thumpnail TEXT)''',
+        );
+      },
+    );
+  }
+}
