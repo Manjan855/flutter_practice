67b412c6e5ce91db778566def9121afaf539008f
Manjan855 <shahimanjan08@gmail.com>
2026-08-22 23:39:29 +0545

    payesewa

--- lib/features/products/presentation/screens/esewa_payment_screen.dart
+++ lib/features/products/presentation/screens/esewa_payment_screen.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/features/products/domain/entities/product_entity.dart';
 
 class EsewaPaymentScreen extends StatelessWidget {
-  const EsewaPaymentScreen({super.key});
+  final ProductEntity vehicle;
+  const EsewaPaymentScreen({super.key, required this.vehicle});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final paymentData = PaymentData(
+      amount: vehicle.price.toString(),
+      taxAmount: '0',
+      totalAmount:vehicle.price.toString(),
+      productCode: 'EPAYTEST',
+       successUrl: 'https://developer.esewa.com.np/success',
+      failureUrl: 'https://developer.esewa.com.np/failure',
+      secretKey: '8gBm/:&EnhH.1/q',
+    );
+    return Scaffold(
+      appBar: AppBar(title: Text('Pay With Esewa'),),
+      body: Center(child: ElevatedButton(onPressed: (){}, child: Text('Pay With Esewa')),),
+    );
   }
 }
